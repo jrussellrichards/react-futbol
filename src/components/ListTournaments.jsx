427b53e3c2830bf0382224b10c5cf1e8be0c1e59
javier <richards.javier@gmail.com>
2020-01-05 17:25:12 -0300

    se mejoran los elementos lista

--- src/components/ListTournaments.jsx
+++ src/components/ListTournaments.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,7 +18,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function SelectedListItem() {
   const classes = useStyles();
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
@@ -26,33 +26,87 @@ export default function SelectedListItem() {
 
   return (
     <div className={classes.root}>
-  
-      <List component="nav"  >
-        <ListItem
-          button
-          selected={selectedIndex === 1}
-          onClick={event => handleListItemClick(event, 1)}
-        >
-          <ListItemText primary="Copa humanidades" />
-        </ListItem>
+   <List>
+  <ListItem 
+  button
+  selected={selectedIndex === 1}
+  onClick={event => handleListItemClick(event, 1)}
+  alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="La liga"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                Informática: +56951097841
+              </Typography>
+              {" — Solo para alumnos de la carrera ingenieria civil informática…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+        <Divider />
+  <ListItem 
+  button
+  selected={selectedIndex === 2}
+  onClick={event => handleListItemClick(event, 2)}
+  alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Copa humanidades"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                Diego Espinoza: +56951099954
+              </Typography>
+              {" — Inscripción para la liguilla de clasificación"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+        <Divider />
+  <ListItem 
+  button
+  selected={selectedIndex === 3}
+  onClick={event => handleListItemClick(event, 3)}
+  alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Copa de campeones"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                Joaquin Cardenas: +56951243261
+              </Typography>
+              {" — Solo para los campeones de cada liga en su carrera"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
         <Divider />
 
-        <ListItem
-          button
-          selected={selectedIndex === 2}
-          onClick={event => handleListItemClick(event, 2)}
-        >
-          <ListItemText primary="Mundial de clubes" />
-        </ListItem>
-        <Divider />
-
-        <ListItem
-          button
-          selected={selectedIndex === 3}
-          onClick={event => handleListItemClick(event, 3)}
-        >
-          <ListItemText primary="Interfacultades" />
-        </ListItem>
+     
       </List>
     </div>
   );
