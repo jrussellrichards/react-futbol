814136e04e91ee46eeca43eed3d33810e653ad5d
javier <richards.javier@gmail.com>
2020-01-05 23:24:24 -0300

    funciona modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,81 +5,100 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import { useSpring, animated } from 'react-spring';
+import Tournament from './Tournament.jsx'
+
 
 const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
 }));
 
 const Fade = React.forwardRef(function Fade(props, ref) {
-  const { in: open, children, onEnter, onExited, ...other } = props;
-  const style = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: open ? 1 : 0 },
-    onStart: () => {
-      if (open && onEnter) {
-        onEnter();
-      }
-    },
-    onRest: () => {
-      if (!open && onExited) {
-        onExited();
-      }
-    },
-  });
+    const { in: open, children, onEnter, onExited, ...other } = props;
+    const style = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: open ? 1 : 0 },
+        onStart: () => {
+            if (open && onEnter) {
+                onEnter();
+            }
+        },
+        onRest: () => {
+            if (!open && onExited) {
+                onExited();
+            }
+        },
+    });
 
-  return (
-    <animated.div ref={ref} style={style} {...other}>
-      {children}
-    </animated.div>
-  );
+    return (
+        <animated.div ref={ref} style={style} {...other}>
+            {children}
+        </animated.div>
+    );
 });
 
 export default function SpringModal(props) {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+    const handleOpen = () => {
+        setOpen(true);
+        console.log("hola")
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div>
-      <button type="button" onClick={handleOpen}>
-      {props.name_button}
-      </button>
-      <Modal
-        aria-labelledby="spring-modal-title"
-        aria-describedby="spring-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="spring-modal-title">Spring modal</h2>
-            <p id="spring-modal-description">react-spring animates me.</p>
-          </div>
-        </Fade>
-      </Modal>
-    </div>
-  );
+    return (
+        <div>
+            <button
+                onClick={handleOpen}>
+                <Tournament
+                    type="button"
+                    name={props.name}
+                    description={props.description}
+                    contact_name={props.contact_name}
+                    contact_number={props.contact_number}
+                    date_tournament = {props.date_tournament}
+                    sport= {props.sport}
+                    img= {props.img}
+                />
+            </button>
+
+            {/* < type="button" onClick={handleOpen}> */}
+
+            {/* </button> */}
+            <Modal
+                aria-labelledby="spring-modal-title"
+                aria-describedby="spring-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <h2 id="spring-modal-title">{props.name}</h2>
+                        <p id="spring-modal-description">{props.description}</p>
+                        <p id="spring-modal-name_contact">Nombre de contacto: {props.contact_name}</p>
+                        <p id="spring-modal-number_contact">Nùmero de contacto: {props.contact_number}</p>
+                    </div>
+                </Fade>
+            </Modal>
+        </div>
+    );
 }
--- src/components/Tournament.jsx
+++ src/components/Tournament.jsx
@@ -8,6 +8,7 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 
+
 const useStyles = makeStyles(theme => ({
     root: {
       width: '100%',
@@ -18,7 +19,7 @@ const useStyles = makeStyles(theme => ({
   
 
   
-export default function SelectedItem() {
+export default function SelectedItem(props) {
     const classes = useStyles();
     const [selectedIndex, setSelectedIndex] = React.useState(0);
   
@@ -32,13 +33,15 @@ export default function SelectedItem() {
     <ListItem 
     button
     selected={selectedIndex === 3}
-    onClick={event => handleListItemClick(event, 3)}
+    onClick={event => handleListItemClick(event, 1)}
     alignItems="flex-start">
           <ListItemAvatar>
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            <Avatar alt="Travis Howard" src={props.img} />
           </ListItemAvatar>
           <ListItemText
-            primary="Copa de campeones"
+            primary={
+                props.name
+            }
             secondary={
               <React.Fragment>
                 <Typography
@@ -47,9 +50,9 @@ export default function SelectedItem() {
                   className={classes.inline}
                   color="textPrimary"
                 >
-                  Joaquin Cardenas: +56951243261
-                </Typography>
-                {" — Solo para los campeones de cada liga en su carrera"}
+                  {props.date_tournament}
+                </Typography> <br></br>
+                {props.sport}
               </React.Fragment>
             }
           />
