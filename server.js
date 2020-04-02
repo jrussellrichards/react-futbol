34cb60db22cb38665d806866d9ae2dc77add6714
javman92 <richards.javier@gmail.com>
2020-04-01 23:27:47 -0300

    se agrega server.js para leer en produccion

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+  next();
+});
+
+app.use(express.static('dist'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/',function(req,res) {
+    res.sendFile(__dirname + '/dist/');
+  });
+
