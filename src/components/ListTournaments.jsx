db1c8b1cf2adb16276919025d2025d47dd91d336
javman92 <richards.javier@gmail.com>
2020-04-02 23:36:49 -0300

    se seleccionan correctamente los torneos, aunque quizás se pueda mejorar con mejores prácticas la manera en que se está haciendo. Revisar listTournaments y comprobar si el if está bien hecho

--- src/components/ListTournaments.jsx
+++ src/components/ListTournaments.jsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import Tournament from "./Card";
 import "../resources/styles/grid.css";
 const axios = require("axios").default;
 
-var tournaments = [];
-
-const getTournaments = sport => {
-  axios
-    .get("https://futbol-back.herokuapp.com/tournaments/sport/" + sport)
-    .then(function(response) {
-      // handle success
-      tournaments = response.data;
-      console.log(tournaments);
-    })
-    .catch(function(error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function() {
-      // always executed
-    });
-};
+var selected_sport = '0'
 
 export default function ListTournaments(props) {
-  
-getTournaments(props.sport)
+const [tournaments,setTournaments] = useState([])
 
+  const getTournaments = sport => {
+    axios
+      .get("https://futbol-back.herokuapp.com/tournaments/sport/" + sport)
+      .then(function(response) {
+        // handle success
+        // tournaments = response.data;
+        if (props.sport!= selected_sport){
+          console.log(selected_sport)
+          selected_sport = props.sport
+          setTournaments(response.data)
+
+        }
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function() {
+        // always executed
+      });
+  };
+
+getTournaments(props.sport)
 
   return (
     <div className="tournamentsWrapper">
