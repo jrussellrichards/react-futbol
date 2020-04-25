37bfcfe484643d2509321b8aaeb98ec3208a53d6
javier <richards.javier@gmail.com>
2020-04-25 17:16:14 -0400

    algo

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,41 @@ const path = require("path");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        loader: [MiniCSSExtractPlugin.loader, "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
+    mode: "development",
+    resolve: {
+        extensions: [".js", ".jsx"],
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new MiniCSSExtractPlugin(),
-  ],
-  devServer: {
-    historyApiFallback: true,
-  },
-  externals: {
-    // global app config object
-    config: JSON.stringify({
-      apiUrl: "http://localhost:5000",
-    }),
-  },
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: [MiniCSSExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+        alias: {
+            "@": path.resolve(__dirname, "src/"),
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new MiniCSSExtractPlugin(),
+    ],
+    devServer: {
+        historyApiFallback: true,
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: "http://localhost:5000",
+            // apiUrl: "https://futbol-back.herokuapp.com/",
+        }),
+    },
 };
